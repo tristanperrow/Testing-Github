6a11266a38a32015f3a773d830cee14fe71edac4
Tristan Perrow <tsperrow49@gmail.com>
2024-12-26 17:24:34 -0500

    feat: a lot of stuff lmao
    jsut did a lot... this is testing anyways so whatever

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,10 +1,11 @@
 import * as server from "@minecraft/server"
+import * as ui from "@minecraft/server-ui"
 
 let dim = server.world.getDimension("overworld");
 
 let useMap = new Map();
 
-let damage = 20;
+let damage = 150;
 let explosionSize = 12;
 
 function spawnOnLine(vec1, vec2, numParticles) {
@@ -70,9 +71,11 @@ server.world.afterEvents.itemStartUse.subscribe((event) => {
     let loc = event.source.location;
     let p = Math.random() * 0.3 + 0.85;
 
+    let player = event.source;
+
     dim.runCommand(`playsound nano.cannon.charge @a[r=50] ${loc.x} ${loc.y} ${loc.z} 1.0 ${p}`)
 
-    event.source.playSound("nano.cannon.charge", {
+    player.playSound("nano.cannon.charge", {
         pitch: p,
         volume: 0.55
     })
@@ -80,12 +83,18 @@ server.world.afterEvents.itemStartUse.subscribe((event) => {
     let mvm = new server.MolangVariableMap();
     mvm.setSpeedAndDirection("variable.velocity", 100, { x: 0, y: 1, z: 0 });
     mvm.setVector3("variable.scale", { x: 0.5, y: 0.5, z: 0.5 })
+    let progress = 0;
     let chargeInterval = server.system.runInterval(() => {
         try {
-            let loc = event.source.getHeadLocation();
-            let pvd = rotateVectorY(event.source.getViewDirection(), 25);
+            let loc = player.getHeadLocation();
+            let pvd = rotateVectorY(player.getViewDirection(), 25);
 
             dim.spawnParticle("minecraft:blue_flame_particle", { x: loc.x + pvd.x, y: loc.y + pvd.y, z: loc.z + pvd.z }, mvm);
+
+            // zoom
+            player.camera.setCamera()
+
+            progress++;
         } catch (e) {
             // out of ticking area
         }
@@ -137,21 +146,23 @@ server.world.afterEvents.itemCompleteUse.subscribe((event) => {
     })
 
     let pvd = player.getViewDirection();
+    let phl = player.getHeadLocation();
 
     // camera shake
     player.runCommand("camerashake add @s 0.5 0.5 positional");
     player.runCommand("camerashake add @s 0.1 0.5 rotational");
 
+    let d = { x: pvd.x * 150, y: pvd.y * 150, z: pvd.z * 150 }
+    let hitBlock = false;
 
     let entities = player.dimension.getEntitiesFromRay(player.getHeadLocation(), pvd, {
-        maxDistance: 150,
+        maxDistance: 191,
         includePassableBlocks: false,
     })
     if (entities.length > 1) {
-        spawnOnLine(player.getHeadLocation(), entities[entities.length - 1].entity.location, Math.floor(entities[1].distance / 3) + 2);
         for (let i = 0; i < entities.length; i++) {
             let entity = entities[i].entity;
-            if (entity.nameTag != player.nameTag) {
+            if (entity.nameTag != player.nameTag && entity.hasComponent(server.EntityComponentTypes.Health)) {
                 entity.applyKnockback(pvd.x, pvd.z, 5, 0.4);
                 entity.applyDamage(damage);
                 // TODO make players take damage with `player.dimension.getPlayers({ location: player.location, maxDistance: 12 });`
@@ -167,17 +178,251 @@ server.world.afterEvents.itemCompleteUse.subscribe((event) => {
         //return;
     }
     let block = player.dimension.getBlockFromRay(player.getHeadLocation(), pvd, {
-        maxDistance: 150,
+        maxDistance: 191,
         includePassableBlocks: false,
     })
     if (block) {
-        // spawn particles
-        spawnOnLine(player.getHeadLocation(), block.block.location, Math.floor(distance(player.getHeadLocation(), block.block.location) / 3) + 2);
+        d = block.block.location;
+        hitBlock = true;
         // logic
-        dim.createExplosion(block.block.location, explosionSize, {
-            breaksBlocks: true,
-            causesFire: true,
-            source: player
-        })
+        try {
+            dim.createExplosion(block.block.location, explosionSize, {
+                breaksBlocks: true,
+                causesFire: true,
+                source: player,
+            })
+        } catch (e) {
+            // out of range
+        }
     }
+    let result = (hitBlock) ? d : { x: phl.x + d.x, y: phl.y + d.y, z: phl.z + d.z };
+    spawnOnLine(phl, result, Math.floor(distance(player.getHeadLocation(), result) / 3) + 2);
 })
+
+//
+
+class Slider {
+    constructor(min = 0, max = 20, num = 5, step = 1) {
+        this.min = min;
+        this.max = max;
+        this.num = num;
+        this.step = step;
+    }
+}
+
+class Dropdown {
+    constructor(options = ["placeholder"]) {
+        this.options = options;
+        this.selected = 0;
+    }
+}
+
+class Toggle {
+    constructor(bool = false) {
+        this.bool = bool;
+    }
+}
+
+class ToggleGroup {
+    constructor(names = []) {
+        this.toggles = {};
+        for (let name of names) {
+            this.toggles[name] = new Toggle(false);
+        }
+    }
+
+    resetToggleValues(names) {
+        for (let name of names) {
+            this.toggles = {};
+            this.toggles[name] = new Toggle(false);
+        }
+    }
+
+    getToggleValues() {
+        let values = {};
+        for (let [key, toggle] of Object.entries(this.toggles)) {
+            values[key] = toggle.bool;
+        }
+        return values;
+    }
+}
+
+class TextInput {
+    constructor(text = "") {
+        this.text = text;
+    }
+}
+
+const structuresNameList = ["Structure1", "Structure2", "Structure3"];
+
+// Updated list structure for the example
+var list = {
+    "ITEMS": {
+        "Effect Accuracy": new Slider(0.05, 1.0, 0.5, 0.05),
+        "Hit Effect Length": new Slider(0.05, 2.0, 0.5, 0.05),
+        "Lingering Effect Length": new Slider(2.0, 8.0, 4.0, 0.25),
+        "Gun Damage": new Slider(1, 20, 7, 1),
+        "Gun Hardcore Mode?": new Toggle(false),
+    },
+    "MAZE GENERATION": {
+        "Maze Size": new Slider(29, 199, 123, 2),
+        "Player Count": new Slider(2, 19, 2, 1),
+        "Wall Type": new Dropdown(["glass", "stonebrick"]),
+        "Water Zone": new Toggle(false),
+        "Loot Type": new Dropdown(["weak", "strong", "blicky"]),
+    },
+    "TOWERS": {
+        "Tower Difficulty": new Dropdown(["custom", "easy", "hard", "both"]),
+        "EASY": new ToggleGroup(structuresNameList),
+        "HARD": {
+            "tower3": new Toggle(false),
+            "tower4": new Toggle(false),
+        },
+        "CENTERS": {
+            "center1": new Toggle(false),
+            "center2": new Toggle(false),
+        }
+    },
+    "GROUP_3": {
+        "Item 4": new Toggle(true),
+        "Item 5": new Toggle(true),
+        "Test Slider": new Slider(2.0, 8.0, 4.0, 0.25),
+        "Test Text": new TextInput("Placeholder")
+    }
+};
+
+function showMenu(entity, data, title = "Main Menu", stack = []) {
+    let properties = {};
+    let groups = {};
+
+    for (let [key, value] of Object.entries(data)) {
+        if (typeof value === "object" && !(value instanceof Slider) && !(value instanceof Dropdown) && !(value instanceof Toggle) && !(value instanceof TextInput) || (value instanceof ToggleGroup)) {
+            groups[key] = value;
+        } else {
+            properties[key] = value;
+        }
+    }
+
+    if (Object.keys(groups).length > 0 && Object.keys(properties).length > 0) {
+        let form = new ui.ActionFormData();
+        form.title(title);
+
+        for (let groupName of Object.keys(groups)) {
+            form.button(`${groupName}`);
+        }
+
+        for (let propName of Object.keys(properties)) {
+            form.button(`Edit: ${propName}`);
+        }
+
+        form.button("<< Back");
+
+        form.show(entity).then(response => {
+            if (response.canceled || response.selection === Object.keys(groups).length + Object.keys(properties).length) {
+                if (stack.length > 0) {
+                    let previous = stack.pop();
+                    showMenu(entity, previous.data, previous.title, stack);
+                }
+                return;
+            }
+
+            let index = response.selection;
+
+            if (index < Object.keys(groups).length) {
+                let selectedGroup = Object.keys(groups)[index];
+                stack.push({ data, title });
+                showMenu(entity, groups[selectedGroup], selectedGroup, stack);
+            } else {
+                let selectedProperty = Object.keys(properties)[index - Object.keys(groups).length];
+                let singleProp = { [selectedProperty]: properties[selectedProperty] };
+                stack.push({ data, title });
+                showMenu(entity, singleProp, `Edit: ${selectedProperty}`, stack);
+            }
+        });
+    } else if (Object.keys(groups).length > 0) {
+        let form = new ui.ActionFormData();
+        form.title(title);
+
+        for (let groupName of Object.keys(groups)) {
+            form.button(`${groupName}`);
+        }
+
+        form.button("<< Back");
+
+        form.show(entity).then(response => {
+            if (response.canceled || response.selection === Object.keys(groups).length) {
+                if (stack.length > 0) {
+                    let previous = stack.pop();
+                    showMenu(entity, previous.data, previous.title, stack);
+                }
+                return;
+            }
+
+            let selectedGroup = Object.keys(groups)[response.selection];
+            stack.push({ data, title });
+            showMenu(entity, groups[selectedGroup], selectedGroup, stack);
+        });
+    } else if (Object.keys(properties).length > 0) {
+        let modalForm = new ui.ModalFormData();
+        modalForm.title(title);
+
+        for (let [key, value] of Object.entries(properties)) {
+            if (value instanceof Slider) {
+                modalForm.slider(key, value.min, value.max, value.step, value.num);
+            } else if (value instanceof Toggle) {
+                modalForm.toggle(key, value.bool);
+            } else if (value instanceof Dropdown) {
+                modalForm.dropdown(key, value.options, value.selected);
+            } else if (value instanceof TextInput) {
+                modalForm.textField(key, "", value.text);
+            } else if (value instanceof ToggleGroup) {
+                for (let [toggleKey, toggleValue] of Object.entries(value.toggles)) {
+                    modalForm.toggle(toggleKey, toggleValue.bool);
+                }
+            }
+        }
+
+        modalForm.submitButton("Save");
+
+        modalForm.show(entity).then(response => {
+            if (response.canceled) {
+                let previous = stack.pop();
+                showMenu(entity, previous.data, previous.title, stack);
+                return;
+            }
+
+            let formValues = response.formValues;
+            let i = 0;
+            for (let [key, value] of Object.entries(properties)) {
+                if (value instanceof Slider) {
+                    value.num = formValues[i];
+                } else if (value instanceof Toggle) {
+                    value.bool = formValues[i];
+                } else if (value instanceof Dropdown) {
+                    value.selected = formValues[i];
+                } else if (value instanceof TextInput) {
+                    value.text = formValues[i];
+                } else if (value instanceof ToggleGroup) {
+                    for (let toggleKey of Object.keys(value.toggles)) {
+                        value.toggles[toggleKey].bool = formValues[i];
+                        i++;
+                    }
+                    continue; // Skip incrementing i here since it's done in the loop
+                }
+                i++;
+            }
+
+            let previous = stack.pop();
+            showMenu(entity, previous.data, previous.title, stack);
+        });
+    }
+}
+
+// Entry point for the script
+server.system.afterEvents.scriptEventReceive.subscribe((event) => {
+    if (event.sourceEntity.typeId !== "minecraft:player") return;
+
+    if (event.id === "nano:test") {
+        showMenu(event.sourceEntity, list);
+    }
+});
